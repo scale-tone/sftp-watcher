240fe728a07679dc2638d6350f0cab79eae7b5eb
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2023-08-07 17:37:30 +0200

    migrated to .NET 6

--- SftpToServiceBusQueueWatcherEntity.cs
+++ SftpToServiceBusQueueWatcherEntity.cs
@@ -1,33 +1,33 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
-using Microsoft.Azure.ServiceBus;
-using System.Text;
+using Azure.Messaging.ServiceBus;
 
 namespace SftpWatcher
 {
     // Checks the list of files in an SFTP folder and emits events to Azure Service Bus queue/topic
     public class SftpToServiceBusQueueWatcherEntity: SftpWatcherEntity
     {
-        public SftpToServiceBusQueueWatcherEntity(ICollector<Message> serviceBusCollector)
+        public SftpToServiceBusQueueWatcherEntity(ICollector<ServiceBusMessage> serviceBusCollector)
         {
             this.eventCollector = serviceBusCollector;
         }
 
         protected override void EmitEvent(WhatHappenedEnum eventType, string filePath)
         {
-            this.eventCollector.Add(new Message { 
-                Label = eventType.ToString("g"),
-                Body = Encoding.UTF8.GetBytes(filePath)
+            this.eventCollector.Add(new ServiceBusMessage { 
+                Subject = eventType.ToString("g"),
+                Body = new BinaryData(filePath)
             });
         }
 
-        private readonly ICollector<Message> eventCollector;
+        private readonly ICollector<ServiceBusMessage> eventCollector;
 
         // Required boilerplate
         [FunctionName(nameof(SftpToServiceBusQueueWatcherEntity))]
         public static Task Run([EntityTrigger] IDurableEntityContext ctx,
-            [ServiceBus("%OUTPUT_QUEUE_OR_TOPIC_NAME%", Connection = "SERVICE_BUS_CONN_STRING")] ICollector<Message> storageQueueCollector
+            [ServiceBus("%OUTPUT_QUEUE_OR_TOPIC_NAME%", Connection = "SERVICE_BUS_CONN_STRING")] ICollector<ServiceBusMessage> storageQueueCollector
         ) => ctx.DispatchAsync<SftpToServiceBusQueueWatcherEntity>(storageQueueCollector);
     }
 }
